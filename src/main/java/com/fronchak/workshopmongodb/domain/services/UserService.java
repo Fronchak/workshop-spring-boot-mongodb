957cdcb409800e178976421cacb78524fc9a40da
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-12-25 21:58:48 -0300

    Implemented methods to update and delete user

--- src/main/java/com/fronchak/workshopmongodb/domain/services/UserService.java
+++ src/main/java/com/fronchak/workshopmongodb/domain/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fronchak.workshopmongodb.domain.dtos.user.UserDTO;
 import com.fronchak.workshopmongodb.domain.entities.User;
@@ -34,6 +35,19 @@ public class UserService {
 	public UserDTO save(UserDTO insertDTO) {
 		User entity = new User();
 		mapper.copyDTOToEntity(insertDTO, entity);
+		entity = repository.save(entity);
+		return mapper.convertEntityToDTO(entity);
+	}
+	
+	public void delete(String id) {
+		repository.deleteById(id);
+	}
+	
+	@Transactional
+	public UserDTO update(UserDTO dto, String id) {
+		User entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found by ID: " + id));
+		mapper.copyDTOToEntity(dto, entity);
 		entity = repository.save(entity);
 		return mapper.convertEntityToDTO(entity);
 	}
